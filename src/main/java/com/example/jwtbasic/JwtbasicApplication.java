3aaeed51fc5775f1a68dcd92eb27ed31e1db574f
salem <salem@sgi.ir>
2022-08-06 16:31:27 +0430

    successful init

---
+++ src/main/java/com/example/jwtbasic/JwtbasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtbasicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/jwtbasic/controller/JwtController.java
@@ -0,0 +1,47 @@
+package com.example.jwtbasic.controller;
+
+import com.example.jwtbasic.jwtutils.JwtUserDetailsService;
+import com.example.jwtbasic.jwtutils.TokenManager;
+import com.example.jwtbasic.jwtutils.models.JwtRequestModel;
+import com.example.jwtbasic.jwtutils.models.JwtResponseModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtController {
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenManager tokenManager;
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponseModel> createToken(@RequestBody JwtRequestModel
+                                                request) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new
+                            UsernamePasswordAuthenticationToken(request.getUsername(),
+                            request.getPassword())
+            );
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        final String jwtToken = tokenManager.generateJwtToken(userDetails);
+        return ResponseEntity.ok(new JwtResponseModel(jwtToken));
+    }
+}
